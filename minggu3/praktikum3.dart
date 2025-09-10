bf899cd1c174230c7657868e3a320e65d4fe1fec
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2025-09-10 13:56:32 +0700

    praktikum 3

---
+++ minggu3/praktikum3.dart
@@ -0,0 +1,10 @@
+main() {
+  int index;
+  for (index = 10; index < 27; index++) {
+    if (index == 21)
+      break;
+    else if (index > 1 && index < 7)
+      continue;
+    print(index);
+  }
+}

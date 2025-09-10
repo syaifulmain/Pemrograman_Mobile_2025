a6eb926d69b31db4cf8f0566f8834610fb9c48b7
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2025-09-10 13:44:53 +0700

    praktikum 2

---
+++ minggu3/praktikum2.dart
@@ -0,0 +1,13 @@
+void main() {
+  int counter = 1;
+  while (counter < 33) {
+    print(counter);
+    counter++;
+  }
+
+  counter = 1;
+  do {
+    print(counter);
+    counter++;
+  } while (counter < 77);
+}

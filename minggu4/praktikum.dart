dbb8b3aa7522003e7b502f7f9c221907b8a75074
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2025-09-22 20:38:42 +0700

    praktikum 1

---
+++ minggu4/praktikum.dart
@@ -0,0 +1,22 @@
+void main()
+{
+    // var list = [1, 2, 3];
+    // assert(list.length == 3);
+    // assert(list[1] == 2);
+    // print(list.length);
+    // print(list[1]);
+
+    // list[1] = 1;
+    // assert(list[1] == 1);
+    // print(list[1]);
+
+    var list = [null, 2341720013, "Muhamad Syaifullah", null, null];
+    assert(list.length == 5);
+    assert(list[1] == 2341720013);
+    print(list.length);
+    print(list[1]);
+
+    list[1] = 1;
+    assert(list[1] == 1);
+    print(list[1]);
+}

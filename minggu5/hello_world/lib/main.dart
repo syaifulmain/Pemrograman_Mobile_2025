b6769e4062b993061c3a8b703fd5d145207f45b6
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2025-10-02 18:52:43 +0700

    praktikum 5 Cupertino Button dan Loading Bar

--- minggu5/hello_world/lib/main.dart
+++ minggu5/hello_world/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/basic_widgets/image_widget.dart';
 import 'package:hello_world/basic_widgets/text_widget.dart';
+import 'package:hello_world/basic_widgets/loading_cupertino.dart';
 
 
 void main() {
@@ -33,7 +34,8 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyLoadingCupertino(),
     );
   }
 }
